9398af05a6bcdf812ee69b29747623a854f28bc9
Omer Taniyici <taniyici.omer@gmail.com>
2024-01-07 20:03:16 -0500

    connect services with hardcoded url

--- fraud/src/main/java/com/sampleapp/fraud/FraudController.java
+++ fraud/src/main/java/com/sampleapp/fraud/FraudController.java
@@ -1,7 +1,9 @@
 package com.sampleapp.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public record FraudController(FraudCheckService fraudCheckService) {
@@ -9,6 +11,7 @@ public record FraudController(FraudCheckService fraudCheckService) {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
