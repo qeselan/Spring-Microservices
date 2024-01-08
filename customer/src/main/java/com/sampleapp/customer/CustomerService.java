232f26ea7fbbbb1912c8e04fd9acffc09dd5e1dc
Omer Taniyici <taniyici.omer@gmail.com>
2024-01-07 20:42:14 -0500

    add eureka server

--- customer/src/main/java/com/sampleapp/customer/CustomerService.java
+++ customer/src/main/java/com/sampleapp/customer/CustomerService.java
@@ -14,7 +14,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
 
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId()
+                "http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId()
         );
 
         if (fraudCheckResponse.isFraudster()) {
